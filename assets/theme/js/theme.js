1a18516b4d9e1e6918ff0f431ea19ffce7af5676
teaganbuckley <teaganbuckley96@gmail.com>
2019-05-05 17:45:28 +1000

    overhaul. removal of blog section. error to be resolved in portfolio section where it doesn't scroll down

--- assets/theme/js/theme.js
+++ assets/theme/js/theme.js
@@ -164,7 +164,7 @@
   //Typed Animation
   function initTyped() {
     $("#typed").typed({
-      strings: ["A Designer", "A Freelancer", "A Marketer", "A Developer"],
+      strings: ["A Marketer", "A Marketer", "A Marketer", "A Marketer"],
       // typing speed
       typeSpeed: 300,
       // time before typing starts
